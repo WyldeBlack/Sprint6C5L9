ee656aa235d2bb82e65e8fcf90b8f2d7d88b9947
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2024-11-17 16:41:53 +0000

    Enhance project configuration and setup
    - Update Streamlit configuration with theme customization options
    - Simplify requirements.txt by reducing dependencies
    - Add setup script for environment setup and dependency installation

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get update  # To get the latest package lists
+sudo apt-get upgrade -y  # To get the latest package lists
+make create_venv  # Create virtual environment
+make install  # Install dependencies
